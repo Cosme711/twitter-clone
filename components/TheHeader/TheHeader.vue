17c83945b20dc03d0241651205a05809f145ad9e
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2022-11-01 21:06:37 +0100

    feat: add the header

---
+++ components/TheHeader/TheHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-between w-11/12 mx-auto h-16">
+    <TheHeaderAssetsTitleLogo />
+    <TheHeaderComponentsNavBar />
+    <TheHeaderComponentsUserPanel />
+  </div>
+</template>
