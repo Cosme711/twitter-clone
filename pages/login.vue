ffd59b5318f71af50c4517d68324199271d4661d
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2022-11-01 21:35:53 +0100

    feat: add connect middleware

---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { useUserStore } from "~~/store/user";
+const router = useRouter();
+
+const userStore = useUserStore();
+const login = () => {
+  userStore.user = {
+    name: "Cosme Gressier",
+  };
+  router.push("/");
+};
+</script>
+
+<template>
+  <p @click="login">Login</p>
+</template>
