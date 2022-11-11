398d01012d06d26a2ebaac68990b5717eba06d3d
Cosme Gressier <39126953+Cosme711@users.noreply.github.com>
2022-11-11 17:10:41 +0100

    feat: integration on user panel options

--- components/TheHeader/components/UserPanel.vue
+++ components/TheHeader/components/UserPanel.vue
@@ -1,20 +1,31 @@
 <script lang="ts" setup>
+import { Ref } from "vue";
 import { useUserStore } from "~/store/user";
+
+const optionsIsOpen: Ref<boolean> = ref(false);
 </script>
 
 <template>
   <div class="relative">
-    <div class="flex items-center font-medium text-sm">
+    <div
+      class="flex items-center font-medium text-sm cursor-pointer"
+      @click="optionsIsOpen = !optionsIsOpen"
+    >
       <img
         src="../assets/fake_profile_picture.jpg"
         alt="Fake Profile Picture"
         class="rounded-lg w-7 h-7"
       />
-      <p class="ml-3">{{ useUserStore().user.displayName }}</p>
-      <BaseIconBase name="arrow" size="w-7 h-7" class="ml-1">
+      <p class="ml-3 select-none">{{ useUserStore().user.displayName }}</p>
+      <BaseIconBase
+        name="Arrow Dropdown"
+        size="w-7 h-7"
+        class="ml-1 duration-200"
+        :class="optionsIsOpen ? '' : 'rotate-180'"
+      >
         <AssetsIconsArrowDropDownIcon />
       </BaseIconBase>
     </div>
-    <TheHeaderComponentsUserPanelOptions />
+    <TheHeaderComponentsUserPanelOptions v-if="optionsIsOpen" />
   </div>
 </template>
